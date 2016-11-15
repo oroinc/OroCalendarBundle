75b2c0415ad3e4a7c805068d7c126d09dbd8a1d9
Hryhorii Hrebiniuk <hhrebiniuk@magecore.com>
2016-11-15 20:25:15 +0200

    CRM-6700: Implement RecurrentType switcher

--- Resources/public/js/calendar/event/recurrence/abstract-recurrence-subview.js
+++ Resources/public/js/calendar/event/recurrence/abstract-recurrence-subview.js
@@ -7,11 +7,25 @@ define(function(require) {
     var BaseView = require('oroui/js/app/views/base/view');
 
     AbstractRecurrenceSubview = BaseView.extend(/** @exports AbstractRecurrenceSubview.prototype */{
+        /** @type {boolean} */
+        isActive: true,
+
+        events: {
+            'change :input[data-related-field]': 'updateModel'
+        },
+
         initialize: function(options) {
             if ('relatedFields' in this === false) {
                 throw new Error('Property "relatedFields" should be declare in successor class');
             }
+            _.extend(this, _.pick(options, 'isActive'));
             AbstractRecurrenceSubview.__super__.initialize.call(this, options);
+        },
+
+        render: function() {
+            AbstractRecurrenceSubview.__super__.render.call(this);
+            this.$el.toggle(this.isActive);
+            return this;
         },
 
         /**
@@ -30,11 +44,23 @@ define(function(require) {
         },
 
         getValue: function() {
-            var value = _.pick(this.model.attributes, this.relatedFields);
-            this.dataInputs().each(function() {
-                value[$(this).data('related-field')] = $(this).val() || null;
-            });
+            var value = _.mapObject(_.pick(this.model.defaults, this.relatedFields), _.clone);
+            if (this.isActive) {
+                this.dataInputs().each(function() {
+                    value[$(this).data('related-field')] = $(this).val() || null;
+                });
+            }
             return value;
+        },
+
+        updateModel: function() {
+            this.model.set(this.getValue());
+        },
+
+        toggle: function(state) {
+            this.isActive = state;
+            this.$el.toggle(state);
+            this.updateModel();
         }
     });
 
--- Resources/public/js/calendar/event/recurrence/event-recurrence-view.js
+++ Resources/public/js/calendar/event/recurrence/event-recurrence-view.js
@@ -4,8 +4,14 @@ define(function(require) {
     var EventRecurrenceView;
     var _ = require('underscore');
     var __ = require('orotranslation/js/translator');
-    var template = require('tpl!orocalendar/templates/calendar/event/recurrence/event-recurrence-template.html');
+    var template = require('tpl!orocalendar/templates/calendar/event/recurrence/recurrence.html');
+    var originValuesTemplate =
+        require('tpl!orocalendar/templates/calendar/event/recurrence/recurrence-origin-values.html');
     var BaseView = require('oroui/js/app/views/base/view');
+    var RecurrenceEndsView = require('orocalendar/js/calendar/event/recurrence/recurrence-ends-view');
+    var RecurrenceDailyView = require('orocalendar/js/calendar/event/recurrence/recurrence-daily-view');
+    var RecurrenceWeeklyView = require('orocalendar/js/calendar/event/recurrence/recurrence-weekly-view');
+    var RecurrenceMonthlyView = require('orocalendar/js/calendar/event/recurrence/recurrence-monthly-view');
 
     EventRecurrenceView = BaseView.extend({
         RECURRENCE_REPEATS: {
@@ -18,11 +24,17 @@ define(function(require) {
         /** @type {string} defines name prefix for all form elements that are related to recurrence */
         inputNamePrefixes: '',
 
+        _isCompletelyRendered: false,
+
         template: template,
 
         events: {
             'change [data-name="recurrence-repeat"]': 'onRecurrentToggle',
             'change [data-name="recurrence-repeats"]': 'onRepeatsChange'
+        },
+
+        listen: {
+            'change model': 'renderOriginValues'
         },
 
         initialize: function(options) {
@@ -35,15 +47,68 @@ define(function(require) {
             var data = EventRecurrenceView.__super__.getTemplateData.call(this);
 
             data.cid = this.cid;
-            data.inputNamePrefixes = this.inputNamePrefixes;
-
-            data.repeatsOptions = _.map(_.keys(this.RECURRENCE_REPEATS), function(item) {
+            data.repeatsOptions = _.map(_.keys(RECURRENCE_REPEATS), function(item) {
                 return {
                     value: item,
                     label: __('oro.calendar.event.recurrence.repeat.' + item),
                     selected: RECURRENCE_REPEATS[item].indexOf(data.recurrenceType) !== -1
                 };
             });
+
+            return data;
+        },
+
+        render: function() {
+            EventRecurrenceView.__super__.render.call(this);
+
+            // if recurrence model is not empty
+            if (this.model.toJSON()) {
+                delete this._isCompletelyRendered;
+                this.renderSubviews();
+            }
+
+            this.renderOriginValues();
+
+            return this;
+        },
+
+        renderSubviews: function() {
+            var repeatViewName = this.getRepeatViewName(this.model.get('recurrenceType')) || 'daily';
+
+            this.subview('ends', new RecurrenceEndsView({
+                autoRender: true,
+                el: this.findElement('ends'),
+                model: this.model
+            }));
+
+            this.subview('daily', new RecurrenceDailyView({
+                autoRender: true,
+                isActive: repeatViewName === 'daily',
+                el: this.findElement('daily'),
+                model: this.model
+            }));
+
+            this.subview('weekly', new RecurrenceWeeklyView({
+                autoRender: true,
+                isActive: repeatViewName === 'weekly',
+                el: this.findElement('weekly'),
+                model: this.model
+            }));
+
+            this.subview('monthly', new RecurrenceMonthlyView({
+                autoRender: true,
+                isActive: repeatViewName === 'monthly',
+                el: this.findElement('monthly'),
+                model: this.model
+            }));
+
+            this._isCompletelyRendered = true;
+        },
+
+        getOriginValuesTemplateData: function() {
+            var data = EventRecurrenceView.__super__.getTemplateData.call(this);
+
+            data.inputNamePrefixes = this.inputNamePrefixes;
             data.recurrenceTypeOptions = _.map(this.model.RECURRENCE_TYPES, function(item) {
                 return {
                     value: item,
@@ -69,17 +134,48 @@ define(function(require) {
             return data;
         },
 
+        renderOriginValues: function() {
+            var html = '';
+            if (this.model.toJSON()) {
+                html = originValuesTemplate(this.getOriginValuesTemplateData());
+            }
+            this.findElement('origin-values').html(html);
+        },
+
         findElement: function(shortName) {
             return this.$('[data-name="recurrence-' + shortName + '"]');
         },
 
         onRecurrentToggle: function(e) {
+            if (!this._isCompletelyRendered) {
+                this.renderSubviews();
+            }
+
             this.findElement('settings').toggle(e.target.checked);
             this.$el.trigger('content:changed');
         },
 
-        onRepeatsChange: function() {
-            // @todo switch to proper Recurrence{Repeat}View
+        onRepeatsChange: function(e) {
+            var repeatViewName = e.target.value;
+            // turn off all repeat view
+            _.each(_.keys(this.RECURRENCE_REPEATS), function(name) {
+                var subview = this.subview(name);
+                if (subview && subview.isActive && repeatViewName !== name) {
+                    subview.toggle(false);
+                }
+            }, this);
+
+            // turn on proper repeat view
+            var subview = this.subview(repeatViewName);
+            if (subview) {
+                subview.toggle(true);
+            }
+        },
+
+        getRepeatViewName: function(repeatType) {
+            return _.findKey(this.RECURRENCE_REPEATS, function(repeatTypes) {
+                return repeatTypes.indexOf(repeatType) !== -1;
+            });
         }
     });
 
