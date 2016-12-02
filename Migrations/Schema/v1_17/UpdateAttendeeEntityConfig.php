da4771d7235c2938dd3b6b9e93cd8819331a8cd8
Ievgenii Myshusta <imyshusta@magecore.com>
2016-12-02 14:44:36 +0200

    BAP-12901: Remove ownership from Attendee entity - fixed notice error on install

--- Migrations/Schema/v1_17/UpdateAttendeeEntityConfig.php
+++ Migrations/Schema/v1_17/UpdateAttendeeEntityConfig.php
@@ -45,19 +45,21 @@ class UpdateAttendeeEntityConfig extends ParametrizedMigrationQuery
 
         // prepare update query
         $rows = $this->connection->fetchAll($query, $params, $types);
-        $row = $rows[0];
-        $data = $this->connection->convertToPHPValue($row['data'], 'array');
-        if (isset($data['security'])) {
-            unset($data['security']);
-            $id = $row['id'];
-            $updateQuery = [
-                'UPDATE oro_entity_config SET data = :data WHERE id = :id',
-                ['id' => $id, 'data' => $data],
-                ['id' => Type::INTEGER, 'data' => Type::TARRAY]
-            ];
-            $this->logQuery($logger, $updateQuery[0], $updateQuery[1], $updateQuery[2]);
-            if (!$dryRun) {
-                $this->connection->executeUpdate($updateQuery[0], $updateQuery[1], $updateQuery[2]);
+        if (count($rows) > 0) {
+            $row = $rows[0];
+            $data = $this->connection->convertToPHPValue($row['data'], 'array');
+            if (isset($data['security'])) {
+                unset($data['security']);
+                $id = $row['id'];
+                $updateQuery = [
+                    'UPDATE oro_entity_config SET data = :data WHERE id = :id',
+                    ['id' => $id, 'data' => $data],
+                    ['id' => Type::INTEGER, 'data' => Type::TARRAY]
+                ];
+                $this->logQuery($logger, $updateQuery[0], $updateQuery[1], $updateQuery[2]);
+                if (!$dryRun) {
+                    $this->connection->executeUpdate($updateQuery[0], $updateQuery[1], $updateQuery[2]);
+                }
             }
         }
     }
