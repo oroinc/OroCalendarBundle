c5ef371e0d39da5c2a3fcea0c77840734c592207
Ievgenii Myshusta <imyshusta@magecore.com>
2016-12-21 19:13:28 +0200

    CRM-6780: Show recurring events of system calendar on My Calendar [Backend] - fixed moving calendar event exceptions between system calendar and user calendar

--- Manager/CalendarEvent/UpdateExceptionManager.php
+++ Manager/CalendarEvent/UpdateExceptionManager.php
@@ -196,7 +196,9 @@ class UpdateExceptionManager
             'title',
             'description',
             'allDay',
-            'backgroundColor'
+            'backgroundColor',
+            'calendar',
+            'systemCalendar'
         ];
     }
 
