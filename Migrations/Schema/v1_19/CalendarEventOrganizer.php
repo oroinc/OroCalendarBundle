29d6389e6a510b292e0ed3d6fb80ad1463e5d476
Marcin Łukaszewicz <mlukaszewicz@oroinc.com>
2017-11-10 15:59:54 +0100

    BAP-15903: Add organizer fields (#14631)
    * BAP-15903: Add organizer fields
    - added fields to entity
    - created migration
    - updated installer

---
+++ Migrations/Schema/v1_19/CalendarEventOrganizer.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Oro\Bundle\CalendarBundle\Migrations\Schema\v1_19;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Schema\Schema;
+
+use Oro\Bundle\EntityBundle\ORM\DatabasePlatformInterface;
+use Oro\Bundle\MigrationBundle\Migration\Extension\DatabasePlatformAwareInterface;
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+
+class CalendarEventOrganizer implements Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function up(Schema $schema, QueryBag $queries)
+    {
+        $table = $schema->getTable('oro_calendar_event');
+        if (!$table->hasColumn('is_organizer')) {
+            $table->addColumn('is_organizer', 'boolean', ['notnull' => false]);
+        }
+        if (!$table->hasColumn('organizer_user_id')) {
+            $table->addColumn('organizer_user_id', 'integer', ['notnull' => false]);
+            $table->addForeignKeyConstraint(
+                $schema->getTable('oro_user'),
+                ['organizer_user_id'],
+                ['id'],
+                ['onUpdate' => null, 'onDelete' => 'SET NULL']
+            );
+        }
+        if (!$table->hasColumn('organizer_email')) {
+            $table->addColumn('organizer_email', 'string', ['notnull' => false, 'length' => 255]);
+        }
+        if (!$table->hasColumn('organizer_display_name')) {
+            $table->addColumn('organizer_display_name', 'string', ['notnull' => false, 'length' => 255]);
+        }
+        if (!$table->hasIndex('organizer_user_id')) {
+            $table->addIndex(['organizer_user_id']);
+        }
+    }
+}
