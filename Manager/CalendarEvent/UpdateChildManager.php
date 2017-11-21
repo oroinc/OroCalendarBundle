2b59c197dd3ed8d37a2ef7e9548d9828160295d9
Daniel Nahrebecki <dnahrebecki@oroinc.com>
2017-11-21 16:32:33 +0100

    Revert "BAP-16049: Event title was updated for attendees in case when Organizer sync data - copy basic fields only in case they are not filled already"
    This reverts commit 8141e45

--- Manager/CalendarEvent/UpdateChildManager.php
+++ Manager/CalendarEvent/UpdateChildManager.php
@@ -288,17 +288,14 @@ class UpdateChildManager
      *
      * @param CalendarEvent $parent
      * @param CalendarEvent $child
-     *
-     * @SuppressWarnings(PHPMD.NPathComplexity) It is just data copy from one object to another
      */
     protected function updateAttendeeCalendarEvent(CalendarEvent $parent, CalendarEvent $child)
     {
-        // copy basic data only in case child doesn't have it
-        $child->setTitle($child->getTitle() ?: $parent->getTitle())
-            ->setDescription($child->getDescription() ?: $parent->getDescription())
-            ->setStart($child->getStart() ?: $parent->getStart())
-            ->setEnd($child->getEnd() ?: $parent->getEnd())
-            ->setAllDay($child->getAllDay() ?: $parent->getAllDay());
+        $child->setTitle($parent->getTitle())
+            ->setDescription($parent->getDescription())
+            ->setStart($parent->getStart())
+            ->setEnd($parent->getEnd())
+            ->setAllDay($parent->getAllDay());
 
         if ($parent->isCancelled()) {
             $child->setCancelled(true);
