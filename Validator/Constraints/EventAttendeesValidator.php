beda5a7784111f092977e576877df630191f1f62
Andrew Fetisov <afetisov@oroinc.com>
2018-01-04 19:57:37 +0200

    BB-12752: GET state-changing requests on front side. Mass actions (#15796)
    - added requestType and allowedRequestTypes option to datagrid_options.mass_action configuration
    - added validations: current request compared to allowedRequestTypes,
    exception if wrong http method set
    - modify js to use requestType option
    - removed duplicated route oro_datagrid_front_mass_action
    - configure delete mass action routes to use POST, DELETE methods only
    - updated documentation
    - updated unit and functional tests

--- Validator/Constraints/EventAttendeesValidator.php
+++ Validator/Constraints/EventAttendeesValidator.php
@@ -9,7 +9,7 @@ use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 
 use Oro\Bundle\CalendarBundle\Entity\CalendarEvent;
-use Oro\Bundle\CalendarBundle\Entity\Attendee;
+use Oro\Bundle\CalendarBundle\Entity\Attendee as AttendeeEntity;
 use Oro\Bundle\CalendarBundle\Entity\Repository\AttendeeRepository;
 
 class EventAttendeesValidator extends ConstraintValidator
@@ -28,11 +28,11 @@ class EventAttendeesValidator extends ConstraintValidator
     }
 
     /**
-     * @param Attendee $attendee1
-     * @param Attendee $attendee2
+     * @param AttendeeEntity $attendee1
+     * @param AttendeeEntity $attendee2
      * @return int
      */
-    private function sortAttendees(Attendee $attendee1, Attendee $attendee2)
+    private function sortAttendees(AttendeeEntity $attendee1, AttendeeEntity $attendee2)
     {
         return strcmp($attendee1->getEmail(), $attendee2->getEmail());
     }
@@ -68,7 +68,7 @@ class EventAttendeesValidator extends ConstraintValidator
     private function getRepository(): ObjectRepository
     {
         return $this->managerRegistry
-            ->getManagerForClass(Attendee::class)
-            ->getRepository(Attendee::class);
+            ->getManagerForClass(AttendeeEntity::class)
+            ->getRepository(AttendeeEntity::class);
     }
 }
