b897480a93b19d914e7b8e0b49034019051baa77
Dmytro Fedorenko <dfedorenko@oroinc.com>
2019-01-28 16:52:36 +0200

    BAP-18190: Incorrect display of all-day calendar events (#21830)
    * BAP-18190: Incorrect display of all-day calendar events
    - set end day as inclusive day
    
    * BAP-18190: Incorrect end date of all-day calendar events
    - fixed creation of one all-day event
    - fixed manipulation with dates in all-day event creation
    
    * BAP-18190: Incorrect end date of all-day calendar events
    - behat coverage for additional cases of creation of one all-day event in week view and manipulation with dates in non all-day event creation

---
+++ Resources/public/js/fullcalendar.js
@@ -0,0 +1,15 @@
+define(function(require) {
+    'use strict';
+
+    var $ = require('jquery');
+    require('fullcalendar');
+
+    $.fullCalendar.Calendar.prototype.normalizeEvent = function(event) {
+        // Restore time in end day for all-day event, because end day must be inclusive
+        // Time was stripped inside FullCalendar library by normalizeEventTimes method
+        if (event.allDay && event.end) {
+            event.end.time(event.endTime);
+            event._end.time(event.endTime);
+        }
+    };
+});
