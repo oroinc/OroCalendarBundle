53398d92269f23d2a690f0f82b34be1ee7cd6d94
DmitriyDemeshko <39055833+DmitriyDemeshko@users.noreply.github.com>
2019-02-10 16:39:05 -0500

    BAP-17654: Upgrade Doctrine bundles to latest stable patch versions (#22237)
    - removed checkParameter() with QueryBuilderUtil for query builders and DQL

--- Ownership/CalendarOwnerAssignmentChecker.php
+++ Ownership/CalendarOwnerAssignmentChecker.php
@@ -4,8 +4,10 @@ namespace Oro\Bundle\CalendarBundle\Ownership;
 
 use Doctrine\ORM\EntityManager;
 use Oro\Bundle\OrganizationBundle\Ownership\OwnerAssignmentChecker;
-use Oro\Component\DoctrineUtils\ORM\QueryBuilderUtil;
 
+/**
+ * Check calendar's owner assignment algorithm
+ */
 class CalendarOwnerAssignmentChecker extends OwnerAssignmentChecker
 {
     /**
@@ -19,8 +21,6 @@ class CalendarOwnerAssignmentChecker extends OwnerAssignmentChecker
             ->createQueryBuilder('calendarEvents')
             ->innerJoin('calendarEvents.calendar', 'calendar')
             ->where('calendar.id = entity.id');
-
-        QueryBuilderUtil::checkParameter($qbParam);
 
         // if a default calendar (its name is NULL) has no events assume that it can be deleted
         // without any confirmation and as result we can remove such calendar from assignment list
