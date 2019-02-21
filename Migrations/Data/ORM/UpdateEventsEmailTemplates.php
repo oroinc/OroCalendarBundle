255152e2246f776141ef71c343a839b026ee853b
Yaroslav Bracilo <yarik.br@gmail.com>
2019-02-21 12:57:43 +0200

    BB-16219: Сreate migrations for Email template with changed link
     - unified logic for HashedMigrations
     - added documentation
     - added migration for email templates where twig function oro_get_absolute_url was added

---
+++ Migrations/Data/ORM/UpdateEventsEmailTemplates.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Oro\Bundle\CalendarBundle\Migrations\Data\ORM;
+
+use Oro\Bundle\EmailBundle\Migrations\Data\ORM\AbstractHashEmailMigration;
+use Oro\Bundle\MigrationBundle\Fixture\VersionedFixtureInterface;
+
+/**
+ * Updates email templates to new version matching old versions available for update by hashes
+ */
+class UpdateEventsEmailTemplates extends AbstractHashEmailMigration implements VersionedFixtureInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function getEmailHashesToUpdate(): array
+    {
+        return [
+            'calendar_reminder' => ['c6c1fda896750b5897aa1c1e39131fe7']
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getVersion()
+    {
+        return '1.0';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getEmailsDir()
+    {
+        return $this->container
+            ->get('kernel')
+            ->locateResource('@OroCalendarBundle/Migrations/Data/ORM/data/emails/events');
+    }
+}
