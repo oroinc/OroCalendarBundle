92d4010e459c8508d99ac7ece7674fdeaf931d5d
Andrey Yatsenco <ayatsenko@oroinc.com>
2019-08-08 19:40:46 +0300

    BB-17453 Excessive demo data (#24837)
    * BB-17453 Excessive demo data
    * BAP-19173: Load demo data fixtures with commands oro:migration:data:load or oro:migration:live:demo:data:load fails (#24769)

--- Migrations/Data/B2C/ORM/LoadUsersCalendarData.php
+++ Migrations/Data/B2C/ORM/LoadUsersCalendarData.php
@@ -15,6 +15,9 @@ use Oro\Bundle\UserBundle\Entity\Role;
 use Oro\Bundle\UserBundle\Migrations\Data\ORM\LoadRolesData;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
+/**
+ * Loads new Calendar entities.
+ */
 class LoadUsersCalendarData extends AbstractFixture implements OrderedFixtureInterface
 {
     use WeekendCheckerTrait;
@@ -142,6 +145,7 @@ class LoadUsersCalendarData extends AbstractFixture implements OrderedFixtureInt
         }
 
         $manager->flush();
+        $this->cleanSecurityContext();
     }
 
     /**
