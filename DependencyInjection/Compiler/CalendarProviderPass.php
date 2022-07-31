d0d1b994c08fba101f11ecda10e452c6f2c8ef8d
Vova Soroka <vsoroka@oroinc.com>
2022-08-01 00:39:33 +0300

    BAP-21570: Remove deprecations in DependencyInjection component (#33405)
    - remove Oro\Component\DependencyInjection\Compiler\TaggedServiceTrait::inverseSortByPriorityAndFlatten
    - remove Oro\Component\DependencyInjection\Compiler\PriorityTaggedLocatorTrait::findAndInverseSortTaggedServices
    - remove Oro\Component\DependencyInjection\Compiler\PriorityTaggedLocatorTrait::findAndInverseSortTaggedServicesWithHandler

--- DependencyInjection/Compiler/CalendarProviderPass.php
+++ DependencyInjection/Compiler/CalendarProviderPass.php
@@ -6,6 +6,7 @@ use Oro\Component\DependencyInjection\Compiler\PriorityTaggedLocatorTrait;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\Compiler\ServiceLocatorTagPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
 
 /**
  * Registers all calendar providers.
@@ -17,9 +18,30 @@ class CalendarProviderPass implements CompilerPassInterface
     /**
      * {@inheritdoc}
      */
-    public function process(ContainerBuilder $container)
+    public function process(ContainerBuilder $container): void
     {
-        $services = $this->findAndInverseSortTaggedServices('oro_calendar.calendar_provider', 'alias', $container);
+        $tagName = 'oro_calendar.calendar_provider';
+        $items = [];
+        $taggedServices = $container->findTaggedServiceIds($tagName, true);
+        foreach ($taggedServices as $id => $tags) {
+            foreach ($tags as $attributes) {
+                $items[$this->getPriorityAttribute($attributes)][] = [
+                    $this->getRequiredAttribute($attributes, 'alias', $id, $tagName),
+                    $id
+                ];
+            }
+        }
+
+        $services = [];
+        if ($items) {
+            ksort($items);
+            $items = array_merge(...array_values($items));
+            foreach ($items as [$key, $id]) {
+                if (!isset($services[$key])) {
+                    $services[$key] = new Reference($id);
+                }
+            }
+        }
 
         $container->getDefinition('oro_calendar.calendar_manager')
             ->setArgument('$providerAliases', array_keys($services))
