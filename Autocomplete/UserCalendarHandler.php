58975c3add583cab9162fee6488b8b034476f4f0
Vova Soroka <vsoroka@oroinc.com>
2022-08-20 02:00:12 +0300

    BAP-21625: Refactor Oro\Bundle\UserBundle\Autocomplete\UserAclHandler (#33606)

--- Autocomplete/UserCalendarHandler.php
+++ Autocomplete/UserCalendarHandler.php
@@ -2,101 +2,159 @@
 
 namespace Oro\Bundle\CalendarBundle\Autocomplete;
 
-use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\QueryBuilder;
+use Doctrine\Persistence\ManagerRegistry;
 use Oro\Bundle\AttachmentBundle\Provider\PictureSourcesProviderInterface;
+use Oro\Bundle\CalendarBundle\Entity\Calendar;
+use Oro\Bundle\EntityBundle\Provider\EntityNameResolver;
 use Oro\Bundle\EntityBundle\Tools\EntityRoutingHelper;
-use Oro\Bundle\OrganizationBundle\Entity\Organization;
-use Oro\Bundle\SecurityBundle\Acl\Voter\AclVoterInterface;
+use Oro\Bundle\FormBundle\Autocomplete\SearchHandlerInterface;
+use Oro\Bundle\SecurityBundle\Acl\Extension\EntityAclExtension;
+use Oro\Bundle\SecurityBundle\Acl\Extension\ObjectIdentityHelper;
 use Oro\Bundle\SecurityBundle\Authentication\TokenAccessorInterface;
 use Oro\Bundle\SecurityBundle\ORM\Walker\AclHelper;
-use Oro\Bundle\SecurityBundle\Owner\OwnerTreeProvider;
-use Oro\Bundle\UserBundle\Autocomplete\UserAclHandler;
-use Oro\Bundle\UserBundle\Entity\User;
+use Oro\Bundle\UserBundle\Autocomplete\UserSearchHandler;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
 /**
- * Autocomplete search handler for users with ACL access level protection for calendar package
+ * Autocomplete search handler for user calendars.
  */
-class UserCalendarHandler extends UserAclHandler
+class UserCalendarHandler implements SearchHandlerInterface
 {
-    /** @var AclHelper */
-    protected $aclHelper;
+    private ManagerRegistry $doctrine;
+    private PictureSourcesProviderInterface $pictureSourcesProvider;
+    private AuthorizationCheckerInterface $authorizationChecker;
+    private TokenAccessorInterface $tokenAccessor;
+    private EntityRoutingHelper $entityRoutingHelper;
+    private EntityNameResolver $entityNameResolver;
+    private AclHelper $aclHelper;
 
-    /**
-     * @param EntityManager $em
-     * @param PictureSourcesProviderInterface $pictureSourcesProvider
-     * @param string $className
-     * @param AuthorizationCheckerInterface $authorizationChecker
-     * @param TokenAccessorInterface $tokenAccessor
-     * @param OwnerTreeProvider $treeProvider
-     * @param EntityRoutingHelper $entityRoutingHelper
-     * @param AclHelper $aclHelper
-     * @param AclVoterInterface $aclVoter
-     */
     public function __construct(
-        EntityManager $em,
+        ManagerRegistry $doctrine,
         PictureSourcesProviderInterface $pictureSourcesProvider,
-        $className,
         AuthorizationCheckerInterface $authorizationChecker,
         TokenAccessorInterface $tokenAccessor,
-        OwnerTreeProvider $treeProvider,
         EntityRoutingHelper $entityRoutingHelper,
-        AclHelper $aclHelper,
-        AclVoterInterface $aclVoter = null
+        EntityNameResolver $entityNameResolver,
+        AclHelper $aclHelper
     ) {
-        parent::__construct(
-            $em,
-            $pictureSourcesProvider,
-            $className,
-            $authorizationChecker,
-            $tokenAccessor,
-            $treeProvider,
-            $entityRoutingHelper,
-            $aclVoter
-        );
-
+        $this->doctrine = $doctrine;
+        $this->pictureSourcesProvider = $pictureSourcesProvider;
+        $this->authorizationChecker = $authorizationChecker;
+        $this->tokenAccessor = $tokenAccessor;
+        $this->entityRoutingHelper = $entityRoutingHelper;
+        $this->entityNameResolver = $entityNameResolver;
         $this->aclHelper = $aclHelper;
     }
 
     /**
-     * @param string $query
-     *
-     * @return object|null
+     * {@inheritDoc}
      */
-    protected function searchById($query)
+    public function search($query, $page, $perPage, $searchById = false)
     {
-        return $this->em->getRepository('OroCalendarBundle:Calendar')->findBy(['id' => explode(',', $query)]);
+        [$search, $entityClass, $permission, $entityId, $excludeCurrentUser] = explode(';', $query);
+        $entityClass = $this->entityRoutingHelper->resolveEntityClass($entityClass);
+
+        $hasMore = false;
+        $object = $entityId
+            ? $this->doctrine->getRepository($entityClass)->find((int)$entityId)
+            : ObjectIdentityHelper::encodeIdentityString(EntityAclExtension::NAME, $entityClass);
+        if ($this->authorizationChecker->isGranted($permission, $object)) {
+            if ($searchById) {
+                $results = $this->doctrine->getRepository(Calendar::class)->findBy(['id' => explode(',', $search)]);
+            } else {
+                $page = (int)$page > 0 ? (int)$page : 1;
+                $perPage = (int)$perPage > 0 ? (int)$perPage : 10;
+                $firstResult = ($page - 1) * $perPage;
+                $perPage++;
+
+                $queryBuilder = $this->createQueryBuilder($search);
+                if ($excludeCurrentUser) {
+                    $queryBuilder
+                        ->andWhere('user.id != :userId')
+                        ->setParameter('userId', $this->tokenAccessor->getUser()->getId());
+                }
+                $queryBuilder
+                    ->setFirstResult($firstResult)
+                    ->setMaxResults($perPage);
+                $results = $this->aclHelper->apply($queryBuilder)->getResult();
+
+                $hasMore = count($results) === $perPage;
+                if ($hasMore) {
+                    $results = \array_slice($results, 0, $perPage - 1);
+                }
+            }
+
+            $resultsData = [];
+            foreach ($results as $item) {
+                $resultsData[] = $this->convertItem($item);
+            }
+        } else {
+            $resultsData = [];
+        }
+
+        return [
+            'results' => $resultsData,
+            'more'    => $hasMore
+        ];
     }
 
     /**
-     * {@inheritdoc}
+     * {@inheritDoc}
      */
-    protected function createQueryBuilder()
+    public function getProperties()
     {
-        return $this->em->createQueryBuilder()
-            ->select('calendar, user')
-            ->from('OroCalendarBundle:Calendar', 'calendar')
-            ->innerJoin('calendar.owner', 'user');
+        return ['id'];
     }
 
     /**
-     * {@inheritdoc}
+     * {@inheritDoc}
      */
-    protected function applyAcl(QueryBuilder $queryBuilder, $accessLevel, User $user, Organization $organization)
+    public function getEntityName()
     {
-        return $this->aclHelper->apply($queryBuilder);
+        return Calendar::class;
     }
 
     /**
-     * {@inheritdoc}
+     * {@inheritDoc}
      */
-    public function convertItem($calendar)
+    public function convertItem($item)
     {
-        $result = parent::convertItem($calendar->getOwner());
-        $result['id'] = $calendar->getId();
-        $result['userId'] = $calendar->getOwner()->getId();
+        /** @var Calendar $item */
+        $result = [];
+        $result['id'] = $item->getId();
+        $result['fullName'] = $this->entityNameResolver->getName($item);
+        $result['userId'] = $item->getOwner()->getId();
+        $result['avatar'] = $this->pictureSourcesProvider->getFilteredPictureSources(
+            $item->getOwner()->getAvatar(),
+            UserSearchHandler::IMAGINE_AVATAR_FILTER
+        );
 
         return $result;
+    }
+
+    private function createQueryBuilder(string $search): QueryBuilder
+    {
+        /** @var EntityManagerInterface $em */
+        $em = $this->doctrine->getManagerForClass(Calendar::class);
+        $queryBuilder = $em->createQueryBuilder()
+            ->select('calendar, user')
+            ->from(Calendar::class, 'calendar')
+            ->innerJoin('calendar.owner', 'user');
+        if ($search) {
+            $queryBuilder->where($queryBuilder->expr()->orX(
+                'LOWER(CONCAT(user.firstName, CONCAT(\' \', user.lastName))) LIKE :search',
+                'LOWER(CONCAT(user.lastName, CONCAT(\' \', user.firstName))) LIKE :search',
+                'LOWER(user.username) LIKE :search',
+                'LOWER(user.email) LIKE :search'
+            ));
+            $queryBuilder->setParameter('search', '%' . str_replace(' ', '%', strtolower($search)) . '%');
+        }
+        $queryBuilder
+            ->andWhere('user.enabled = :enabled')
+            ->setParameter('enabled', true);
+
+        return $queryBuilder;
     }
 }
