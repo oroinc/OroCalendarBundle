30ddca8ff96d30c51cb42bb07b97b5c2fb9f26ad
Vova Soroka <vsoroka@oroinc.com>
2023-07-08 18:06:25 +0300

    Replace EntityManager to EntityManagerInterface in some classes in platform package (#36015)

--- Datagrid/MassAction/DeleteMassActionHandler.php
+++ Datagrid/MassAction/DeleteMassActionHandler.php
@@ -2,32 +2,29 @@
 
 namespace Oro\Bundle\CalendarBundle\Datagrid\MassAction;
 
-use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Oro\Bundle\CalendarBundle\Entity\CalendarEvent;
 use Oro\Bundle\CalendarBundle\Manager\CalendarEvent\DeleteManager;
 use Oro\Bundle\DataGridBundle\Extension\MassAction\DeleteMassActionHandler as ParentHandler;
 
+/**
+ * Delete mass action handler for CalendarEvent entity.
+ */
 class DeleteMassActionHandler extends ParentHandler
 {
-    /**
-     * @var DeleteManager
-     */
-    protected $deleteManager;
+    private DeleteManager $deleteManager;
 
-    /**
-     * @param DeleteManager $deleteManager
-     */
-    public function setDeleteManager($deleteManager)
+    public function setDeleteManager(DeleteManager $deleteManager): void
     {
         $this->deleteManager = $deleteManager;
     }
 
     /**
-     * {@inheritdoc}
+     * {@inheritDoc}
      */
-    protected function processDelete($entity, EntityManager $manager)
+    protected function processDelete(object $entity, EntityManagerInterface $manager): void
     {
+        /** @var CalendarEvent $entity */
         $this->deleteManager->deleteOrCancel($entity, true);
-
-        return $this;
     }
 }
